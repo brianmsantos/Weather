0098826ac47f7f295e34fd8627c36ef5b3d6eb8e
Brian Santos <brianmsantos81@gmail.com>
2019-06-23 00:09:38 -0700

    textcontent added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
-    
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
+
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -18,6 +22,11 @@ window.addEventListener('load', ()=>{
             .then(data => {
                 console.log(data);
                 const{temperature, summary} = data.currently;
+                //Set DOM elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+
             })
         });
 
