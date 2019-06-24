f42c31fbae33a450e280065979b5402c9e0aa0d7
Brian Santos <brianmsantos81@gmail.com>
2019-06-23 18:55:39 -0700

    finished weather app using HTML, CSS, Javascript, Skycons, and Darksky API.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ window.addEventListener('load', ()=>{
     let temperatureDescription = document.querySelector(".temperature-description");
     let temperatureDegree = document.querySelector(".temperature-degree");
     let locationTimezone = document.querySelector(".location-timezone");
-
+    let temperatureSection = document.querySelector('.temperature');
+    let temperatureSpan = document.querySelector('.temperature span');
 
 
     if(navigator.geolocation){
@@ -21,15 +22,34 @@ window.addEventListener('load', ()=>{
             })
             .then(data => {
                 // console.log(data);
-                const{temperature, summary} = data.currently;
+                const{temperature, summary, icon} = data.currently;
                 //Set DOM elements from the API
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                //Formula for Celsius
+                let celsius = (temperature - 32) * (5 / 9);
+                //Set Icon function plays here
+                setIcons(icon, document.querySelector(".icon"));
 
+                //Change temperature to Celsius/Farenheit
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    } else { 
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                })
             })
         });
-
-        
+    }
+    function setIcons(icon, iconID){
+        const skycons = new Skycons ({ color: "white"});
+        //changes current property from - to underscores and also uppercases 
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
